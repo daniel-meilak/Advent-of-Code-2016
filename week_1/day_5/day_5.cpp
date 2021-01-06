92b7354f58f578b0385680e2f15c15f17cdcb6a6
Daniel <daniel.mlak@gmail.com>
2021-01-06 15:57:34 +0100

    Finished day 5

--- week_1/day_5/day_5.cpp
+++ week_1/day_5/day_5.cpp
@@ -4,18 +4,56 @@
 #include<algorithm>
 #include<cstdlib>
 #include"../../Utils/utils.h"
+#include"md5.cpp"
 
 int main(){
 
-    // read input into vector of strings.
-    // std::vector<std::string> input = read_input("input", "");
+    std::string input = "uqwqemis";
 
-    // read input into vector of vector of strings.
-    // std::vector<std::string> delimiters = {};
-    // std::vector<std::vector<std::string>> input = read_input_2D("input", delimiters);
+    // index
+    int index = 4000000;
 
-    std::cout << "Answer (part 1): " << std::endl;
-    std::cout << "Answer (part 2): " << std::endl;
+    // passwords and hash
+    std::string password1(8,'x');
+    std::string password2(8,'x');
+    std::string hash = "hash";
+
+    // index for part 1
+    int idx = 0;
+
+    // vector of bool for filled password slots (part 2)
+    std::vector<bool> filled(8,false);
+
+    // loop while filled still has false entries
+    while (std::find(filled.begin(), filled.end(), false) != filled.end()){
+
+        hash = input+std::to_string(index);
+        hash = MD5(hash).hexdigest();
+
+        while (hash.substr(0,5)!="00000"){
+            index++;
+            hash = input + std::to_string(index);
+            hash = MD5(hash).hexdigest();
+        }
+
+        // part 1
+        if (idx < 8){
+            password1[idx] = hash[5];
+            idx++;
+        }
+
+        // part 2
+        int pos = hash[5]-'0';
+        if (pos >= 0 && pos < 8 && !filled[pos]){
+            password2[pos] = hash[6];
+            filled[pos] = true;
+        }
+
+        index++;
+    }
+
+    std::cout << "Answer (part 1): " << password1 << std::endl;
+    std::cout << "Answer (part 2): " << password2 << std::endl;
 
     return 0;
 }
